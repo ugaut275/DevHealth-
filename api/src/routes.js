08771522ad800b6a1cfc616ac61d8e3bcd9068ea
josesanchez2004 <jsanc618@mtroyal.ca>
2024-11-25 20:36:28 -0700

    Created get all users api call, testing

--- api/src/routes.js
+++ api/src/routes.js
@@ -4,4 +4,18 @@ module.exports.register = (app, database) => {
         res.status(200).send("This is running!").end();
     });
 
+    app.get("api/users", async (req,res) => {
+        try {
+            let query;
+            query = database.query('SELECT * FROM user');
+            const records = await query;
+            res.status(200).send(JSON.stringify(records)).end();
+
+        } catch (error) {
+            console.error("Database query failed:", error);
+            res.status(500).json({message: "An error occurred while fetching items."});
+        }
+    })
+
+
 }
